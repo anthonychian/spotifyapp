d33cb565bf1dde6f0bdc1b27a35b5f1dc81a322d
Anthony Chian <anthony@Anthonys-MacBook-Pro.local>
2021-10-05 21:45:45 -0700

    added client

---
+++ client/src/components/NowPlaying.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Palette } from 'react-palette';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+
+export default function NowPlaying(props) {
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: "100%", 
+            width: "100%"
+        }}>
+            <Palette src={props.nowPlaying.image}>
+                {({ data, loading, error }) => (
+                    props.setCurrentColor(data.vibrant)
+                )}
+            </Palette>
+            <ImageListItem sx={{ width: '30%', height: 'auto' }}>
+                <img 
+                    src={`${props.nowPlaying.image}?w=248&fit=crop&auto=format`}
+                    srcSet={`${props.nowPlaying.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={props.nowPlaying.name}
+                    loading="lazy"
+                />
+                <ImageListItemBar
+                    title={props.nowPlaying.name}         
+                    subtitle={props.nowPlaying.artist}
+                    actionIcon={
+                    <IconButton
+                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                        aria-label={`info about ${props.nowPlaying.name}`}
+                    >
+                    </IconButton>
+                    }
+                />
+            </ImageListItem>
+        </div>
+    )
+}
---
+++ client/src/useAuth.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+export default function useAuth(code) {
+  const [accessToken, setAccessToken] = useState();
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:8000/login", { code })
+      .then((response) => {
+
+        // If success then cut the code string from the URL and execute the other thing
+        window.history.pushState({}, null, "/");
+
+        //console.log(response.data);
+        //console.log(response.data.accessToken);
+        setAccessToken(response.data.accessToken);
+
+      })
+      .catch(() => {
+        //   If fail redirect to home page - Login page
+        window.location = "/";
+      });
+  }, [code]);
+
+  return accessToken
+}
