d23b8cef05e8af40b050647645718c083b786173
Anthony Chian <anthony@Anthonys-MacBook-Pro.local>
2021-10-05 17:21:54 -0700

    added existing client and server

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+const spotifyWebApi = require('spotify-web-api-node')
+
+const app = express()
+const port = 8000
+
+app.use(cors()) // To handle cross-origin requests
+app.use(express.json()); // To parse JSON bodies
+
+const credentials = {
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  redirectUri: process.env.REACT_APP_REDIRECT_URI,
+};
+
+app.get('/', (req, res) => {
+  console.log('Hello World!')
+})
+
+app.post('/login', (req,res) => {
+//  setup 
+    let spotifyApi = new spotifyWebApi(credentials)
+
+//  Get the "code" value posted from the client-side and get the user's accessToken from the spotify api     
+    const code = req.body.code
+
+    // Retrieve an access token
+    spotifyApi.authorizationCodeGrant(code).then((data) => {
+
+        // Returning the User's AccessToken in the json formate  
+        res.json({
+            accessToken : data.body.access_token,
+        }) 
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(400)
+    })
+
+})
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
