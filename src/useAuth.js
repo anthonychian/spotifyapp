941ac33c606c8a8a3343e9f2e7044161a63398e8
Anthony Chian <anthony@Anthonys-MacBook-Pro.local>
2021-10-15 05:16:13 -0700

    base url

--- src/useAuth.js
+++ src/useAuth.js
@@ -6,11 +6,12 @@ export default function useAuth(code) {
   // const baseURL = 'http://localhost:8000'
   // const baseURL = process.env.REACT_APP_REDIRECT_URI
   // https://spotify-api-react-app.herokuapp.com:8000/login
+  // https://spotify-api-react-app-server.herokuapp.com/
 
 
   useEffect(() => {
     axios
-      .post(`${process.env.REACT_APP_REDIRECT_URI}:80/login`, { code })
+      .post(`https://spotify-api-react-app-server.herokuapp.com:8000/login`, { code })
       .then((response) => {
         // If success then cut the code string from the URL and execute the other thing
         window.history.pushState({}, null, "/");
